b079a1682cf0dd359f6dcece36bf4fa516b1b876
Sanskar Sisodia <110807171+Sanskar-Sisodia@users.noreply.github.com>
2025-03-10 15:24:49 +0530

    Update CORS.java

--- filxconnect/src/main/java/com/filxconnect/config/CORS.java
+++ filxconnect/src/main/java/com/filxconnect/config/CORS.java
@@ -13,7 +13,8 @@ public WebMvcConfigurer corsConfigurer() {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("https://fil-x-connect-frontend-kaui71xro-sanskar-sisodias-projects.vercel.app/")
+                 .allowedOrigins("https://fil-x-connect-frontend.vercel.app")
+                    .allowedOrigins("https://fil-x-connect-frontend-kaui71xro-sanskar-sisodias-projects.vercel.app")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowCredentials(true);
         }
