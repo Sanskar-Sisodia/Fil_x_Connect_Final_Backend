27f50d2fb7f76153a8b686130092fb470af23f7c
Sanskar Sisodia <110807171+Sanskar-Sisodia@users.noreply.github.com>
2025-03-10 19:43:09 +0530

    Update DefaultController.java

--- filxconnect/src/main/java/com/filxconnect/controller/DefaultController.java
+++ filxconnect/src/main/java/com/filxconnect/controller/DefaultController.java
@@ -1,4 +1,4 @@
-import com.filxconnect.controller;
+package com.filxconnect.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
